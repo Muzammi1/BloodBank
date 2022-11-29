45f5984837c7b87ed83e990f1e0d2054a3d67f98
asp <asp@Aptechnk1.edu>
2022-11-29 20:39:26 +0500

    Initial commit

---
+++ app/src/main/java/com/example/bloodbank/Act_SignUp.java
@@ -0,0 +1,14 @@
+package com.example.bloodbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Act_SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_sign_up);
+    }
+}
