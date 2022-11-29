ca1cdf8de5ef2556a1ce5858e8628c60af3edba1
Muzammil <ajazs8229@gmail.com>
2022-11-29 20:41:30 +0500

    first commit

--- app/src/main/java/com/example/bloodbank/Act_SignUp.java
+++ app/src/main/java/com/example/bloodbank/Act_SignUp.java
@@ -10,5 +10,7 @@ public class Act_SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_sign_up);
+
+        //jhgjgj
     }
 }
